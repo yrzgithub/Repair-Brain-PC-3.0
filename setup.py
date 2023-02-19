f430a17a1bbbfa2c2b6b8b0da6c53c32e1b0057f
yrzgithub <sec20ec044@sairamtap.edu.in>
2023-02-19 09:25:01 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from os import system
+
+requirements_txt = "requirements.txt"
+required_modules = []
+
+with open(requirements_txt,"r") as file:
+    required_modules = file.read().split("\n")
+    file.close()
+
+print("Reuired modules : ",*required_modules,sep="\n",end="\n")
+
+for module in required_modules:
+    print(f"\nInstalling {module}\n")
+    system(f"pip install {module}")
+
+input("\n\nPress enter to exit...")
