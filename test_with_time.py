c56ff136d3d8dd1ed177b3ee858f5b4a997c7a14
yrzgithub <sec20ec044@sairamtap.edu.in>
2023-03-04 12:12:25 +0530

    Repair Brain 2.0

---
+++ test_with_time.py
@@ -0,0 +1,28 @@
+from os import system
+import win32api 
+from datetime import datetime
+
+week_days = ("Mon","Tue","Wed","Thur","Fri","Sat","Sun")
+
+now = datetime.now()
+year = now.year
+month = now.month
+
+win32api.SetSystemTime(year,month,3,1,2,30,3,100)
+n = 28 # feb = 28
+
+print("System date and time changed")
+
+for i in range(n):
+    #input("press enter"
+    now = datetime.now()
+    year = now.year
+    month = now.month
+    day = now.day
+    hour = now.hour
+    minute = now.minute
+    sec = now.second
+    week_day = now.weekday()
+    win32api.SetSystemTime(year,month,week_day,day+1,0,0,0,0)
+    print("Day : ",week_days[datetime.now().weekday()])
+    system("py test.pyw")
